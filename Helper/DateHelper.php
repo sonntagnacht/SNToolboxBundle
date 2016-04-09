662e3ec8f0247f354c5fa815dcce2580a09cd2f7
Max Girkens <maxgirkens@gmail.com>
2016-04-09 09:50:38 +0200

    added datehelper

---
+++ Helper/DateHelper.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * umwelt-online-website
+ * Created by PhpStorm.
+ * File: DateHelper.php
+ * User: con
+ * Date: 10.08.15
+ * Time: 09:04
+ */
+
+namespace SN\ToolboxBundle\Helper;
+
+
+class DateHelper
+{
+
+    /**
+     * converts a string to a datetime object
+     *
+     * @param String|\DateTime $dateStr
+     * @return \DateTime
+     */
+    public static function convertStringToDate($dateStr)
+    {
+        if(is_null($dateStr)) {
+            return;
+        }
+        if (is_string($dateStr)) {
+            $timestamp      = strtotime($dateStr);
+            $date = new \DateTime();
+            $date->setTimestamp($timestamp);
+            return $date;
+        }else if($dateStr instanceof \DateTime){
+            return $dateStr;
+        }else{
+            throw new \InvalidArgumentException(sprintf('String or DateTime required'));
+        }
+    }
+
+}
