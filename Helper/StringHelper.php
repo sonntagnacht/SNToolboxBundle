e4ada7d9b577ba62e3cb282a6699f3b07ccfac98
Conrad Barthelmes <conrad.barthelmes@gmail.com>
2016-11-24 10:09:17 +0100

    added format bytes method

--- Helper/StringHelper.php
+++ Helper/StringHelper.php
@@ -326,4 +326,16 @@ class StringHelper extends Transliterator
             $str);
     }
 
+    /**
+     * @param $size
+     * @param int $precision
+     * @return string
+     */
+    public static function formatBytes($size, $precision = 2)
+    {
+        $base = log($size, 1024);
+        $suffixes = array('', 'K', 'M', 'G', 'T');
+
+        return round(pow(1024, $base - floor($base)), $precision) .' '. $suffixes[floor($base)];
+    }
 }
